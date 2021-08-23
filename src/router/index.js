08c64abcf25a42398b7b9887badc9e078e3a474f
WandererDL2017 <wandererdl@mail.ru>
2021-08-23 16:54:09 +0400

    added pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,8 +7,57 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
-  }
+    meta: {layout: 'main'},
+    component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    meta: {layout: 'empty'},
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    meta: {layout: 'empty'},
+    component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/categories',
+    name: 'Categories',
+    meta: {layout: 'main'},
+    component: () => import('../views/Categories.vue')
+  },
+  {
+    path: '/detail',
+    name: 'Detail',
+    meta: {layout: 'main'},
+    component: () => import('../views/Detail.vue')
+  },
+  {
+    path: '/history',
+    name: 'History',
+    meta: {layout: 'main'},
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/planning',
+    name: 'Planning',
+    meta: {layout: 'main'},
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    meta: {layout: 'main'},
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name: 'Record',
+    meta: {layout: 'main'},
+    component: () => import('../views/Record.vue')
+  },
 ]
 
 const router = new VueRouter({
